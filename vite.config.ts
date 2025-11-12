a3480acadaeae2098b272a589c9a7a23e74ff15b
polter-dev <144177408+polter-dev@users.noreply.github.com>
2025-11-12 18:10:17 -0500

    fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-const repoName = 'polter-dev.github.io'
+const repository = (process.env.GITHUB_REPOSITORY || '').toLowerCase()
+const repoName = repository.split('/')[1] || ''
+const isUserSite = repoName.endsWith('.github.io')
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
+  base: process.env.GITHUB_ACTIONS
+    ? isUserSite
+      ? '/'
+      : `/${repoName}/`
+    : '/',
 })
