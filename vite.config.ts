5836381e80685b522a4aa4fb18a9727ecd09edb3
polter-dev <144177408+polter-dev@users.noreply.github.com>
2025-11-12 18:04:45 -0500

    trying to fix deployement issues for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = 'polter-dev.github.io'
+
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
 })
